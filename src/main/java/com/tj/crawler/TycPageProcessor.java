5015ce82847269a26e3c5676a8b38048fc54a0da
tangjie <tangjie@daojia.com>
2018-10-11 20:15:57 +0800

    add method

--- src/main/java/com/tj/crawler/TycPageProcessor.java
+++ src/main/java/com/tj/crawler/TycPageProcessor.java
@@ -5,8 +5,12 @@ import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.FilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Html;
+import us.codecraft.webmagic.selector.Selectable;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -28,13 +32,14 @@ public class TycPageProcessor implements PageProcessor {
         String headerStr =
                 "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\n" +
                         "Accept-Encoding: gzip, deflate, br\n" +
-                        "Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7\n" +
+                        "Accept-Language: zh-CN,zh;q=0.9\n" +
+                        "Cache-Control: no-cache\n" +
                         "Connection: keep-alive\n" +
-                        "Cookie: aliyungf_tc=AQAAADqiWG6hvQUALiF4alsZ7IWSypE1; channel=baidu; Hm_lvt_52d64b8d3f6d42a2e416d59635df3f71=1538231104,1539187455; sid=s%3A91P2eCg8AkTKaPZkUvLeHCGsE6mTcMFR.tBR2PDinXYRq31opoFO%2BpcZb7m2ayx19XF7zDGqwfac; latest_pos=MmQ1NTg2NDktOTRhOS00ZjM0LWI3YzQtMTQxMjczOGVmYmYy; Hm_lpvt_52d64b8d3f6d42a2e416d59635df3f71=1539187534; responseTimeline=147; _zg=%7B%22uuid%22%3A%20%2216625b7f38aaad-04650d4a3583f6-1130685c-384000-16625b7f38b90%22%2C%22sid%22%3A%201539187455.273%2C%22updated%22%3A%201539187551.703%2C%22info%22%3A%201539187455279%2C%22cuid%22%3A%20%2222edd96c-e83e-4b81-835a-bb2e469cc5ed%22%7D\n" +
-                        "Host: www.qixin.com\n" +
-                        "Referer: https://www.qixin.com/company/9eda1ceb-4d50-4b02-9ef0-ad1437d24f75\n" +
+                        "Cookie: TYCID=128660e0c2f611e8ab81759e05b6e185; undefined=128660e0c2f611e8ab81759e05b6e185; ssuid=2060762260; _ga=GA1.2.1376999845.1538122277; aliyungf_tc=AQAAACCBq3HyJgQAorfGb+83T6OItinz; csrfToken=NymNPgZ3DifLRl5q514QG3SY; bannerFlag=true; _gid=GA1.2.338539365.1538964190; RTYCID=65a26ddaa9e34c6a849dece6586fda8a; CT_TYCID=0db6bbb7a7df49f6a0efd67a0b89d0d9; tyc-user-info=%257B%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODUxNDIzNjAyNSIsImlhdCI6MTUzOTIyMjI2MCwiZXhwIjoxNTU0Nzc0MjYwfQ.rBf6c1v7-IeQX9BxIuIUmm7ZojzU7rfQUe_OY-_1Jz7Ku37XquH_ZACI-0Y3-Cy33YxCXV3I_HTpDdwLhQa62g%2522%252C%2522integrity%2522%253A%25220%2525%2522%252C%2522state%2522%253A%25220%2522%252C%2522redPoint%2522%253A%25220%2522%252C%2522vipManager%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522monitorUnreadCount%2522%253A%252212%2522%252C%2522onum%2522%253A%25220%2522%252C%2522mobile%2522%253A%252218514236025%2522%257D; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODUxNDIzNjAyNSIsImlhdCI6MTUzOTIyMjI2MCwiZXhwIjoxNTU0Nzc0MjYwfQ.rBf6c1v7-IeQX9BxIuIUmm7ZojzU7rfQUe_OY-_1Jz7Ku37XquH_ZACI-0Y3-Cy33YxCXV3I_HTpDdwLhQa62g; cloud_token=b5db61a8212c47ea940716137c2e4f4d; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1539164563,1539165728,1539222249,1539245989; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1539245989; _gat_gtag_UA_123487620_1=1\n" +
+                        "Host: www.tianyancha.com\n" +
+                        "Pragma: no-cache\n" +
                         "Upgrade-Insecure-Requests: 1\n" +
-                        "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.3";
+                        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
 
         String[] headerLines = headerStr.trim().split("\\n");
         for (int i = 0; i < headerLines.length; i++) {
@@ -47,12 +52,30 @@ public class TycPageProcessor implements PageProcessor {
     @Override
     public void process(Page page) {
         System.out.println(page);
+        Html html = page.getHtml();
+        String companyName = html.xpath("//a[@tyc-event-ch=\"CompanySearch.Company\"]").nodes().get(0).xpath("//a/text()").get();
+        List<Selectable> nodes = html.$(".result-list").$(".info").xpath("/div/div").nodes();
+        if(nodes != null && !nodes.isEmpty()) {
+            String legalPersonName = nodes.get(0).xpath("//a/text()").get();
+            String regCapital = nodes.get(1).xpath("//a/text()").get();
+            String regTime = nodes.get(2).xpath("//a/text()").get();
+
+            page.putField("companyName",companyName);
+            page.putField("legalPersonName",legalPersonName);
+            page.putField("regCapital",regCapital);
+            page.putField("regTime",regTime);
+        }
+
     }
 
     public static void main(String[] args) throws InterruptedException {
         logger.info("begin");
 
-        /*TycPageProcessor tycPageProcessor = new TycPageProcessor();
-        Spider.create(tycPageProcessor).addUrl("https://www.qixin.com/search?key=58%E5%88%B0%E5%AE%B6").thread(1).run();*/
+        TycPageProcessor tycPageProcessor = new TycPageProcessor();
+        Spider.create(tycPageProcessor)
+                .addUrl("https://www.tianyancha.com/search?key=%e5%ad%90%e5%bc%b9%e7%9f%ad%e4%bf%a1")
+                .addPipeline(new FilePipeline("e:/result"))
+                .thread(1)
+                .run();
     }
 }
--- src/test/java/com/springapp/mvc/AppTests.java
+++ src/test/java/com/springapp/mvc/AppTests.java
@@ -1,40 +1,38 @@
 package com.springapp.mvc;
 
-import org.junit.Before;
+import org.assertj.core.util.Strings;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
+import us.codecraft.webmagic.selector.Html;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
 @ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
 public class AppTests {
-    private MockMvc mockMvc;
-
-    @SuppressWarnings("SpringJavaAutowiringInspection")
-    @Autowired
-    protected WebApplicationContext wac;
-
-    @Before
-    public void setup() {
-        this.mockMvc = webAppContextSetup(this.wac).build();
-    }
+    private static Logger LOG = LoggerFactory.getLogger(AppTests.class);
 
     @Test
     public void simple() throws Exception {
+        LOG.info("test test");
 
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("hello"));
+    }
+
+    @Test
+    public void testXpath() throws Exception {
+        Path path = Paths.get("E:\\companies\\1044904777028734976.html");
+        String content = Strings.join(Files.readAllLines(path)).with("\n");
+
+        Html html = new Html(content);
+        String s = html.$(".result-list").$(".info").xpath("/div/div").all().get(0);
+        html.xpath("//div[contains(text(),'注册资本')]").all().get(0);
+        LOG.info(s);
+
     }
 }
